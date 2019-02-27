12306a0250b9de90b943f646a5186f21d78f5215
noohes <frostbastard@gmail.com>
2019-02-27 19:04:36 +0900

    raid skill update(time error, modify raid, add model)

--- map/skills_raid.py
+++ map/skills_raid.py
@@ -29,16 +29,17 @@ def make_simple_text_response(text):
 
 # 레이드 시작 가능 시간을 구해주는 함수
 def get_datetime(time, date):
+    # 24시 에러 제거
+    if time[0] == 24:
+        time[0] = 0
     # input을 datetime형식으로 변환
     st = datetime.datetime(date[0], date[1], date[2], time[0], time[1], 0, 0)
-    # 12시 이후에 12시간 방식으로 제보하면 24시간 방식으로 변환해줌
-    if (datetime.datetime.now().time() > datetime.time(12)) and (st.time() < datetime.time(12)):
-        st += timedelta(hours=12)
-        # 0시의 경우에는 하루가 지나야함으로 12시간 추가
-        if st.time() == datetime.time(0):
-            st += timedelta(hours=12)
-
-    return st
+    # st_li는 am, pm, 어제, 오늘, 내일 모두 고려한 선택지
+    st_li = [st, st+timedelta(hours=12), st+timedelta(hours=-12), st+timedelta(days=1), st+timedelta(days=-1), st+timedelta(hours=36), st+timedelta(hours=-36)]
+    # st_li_gap은 현재 시간과 선택지 간의 차를 구함
+    st_li_gap = [abs(t-datetime.datetime.now()) for t in st_li]
+    # 가장 차가 적은 st_li_gap의 인덱스를 st_li로 넘겨서 return
+    return st_li[st_li_gap.index(min(st_li_gap))]
 
 
 # raid_post의 jsonresponse를 만들어주는 함수
@@ -48,7 +49,7 @@ def get_resp(params, raid_ing_object, stt):
         return make_simple_text_response('raid level receive ok')
     elif 'raid_poke_name' in params:
         poke = raid.objects.filter(poke__name=params['raid_poke_name']['value'])
-        raid_ing_object.update(poke=poke[0].id, s_time=stt)
+        raid_ing_object.update(poke=poke[0].id, tier=poke[0].Tier, s_time=stt)
         return make_simple_text_response('raid poke receive ok')
     else: return make_simple_text_response('no raid level or poke')
 
@@ -68,13 +69,13 @@ def raid_post(request):
 
 def get_raid_board(raid_bd):
     text = ""
-    for i in raid_bd:
+    for raid in raid_bd:
         raid_obj = ""
-        if i.poke:
-            raid_obj += str(i.poke)
+        if raid.poke:
+            raid_obj += str(raid.poke)
         else:
-            raid_obj += str(i.tier) + "성"
-        text += str(i.s_time.strftime('%H:%M')) + "~" + str((i.s_time + timedelta(minutes=45)).strftime('%H:%M')) + " " + str(i.gym) + " " + raid_obj + "\n"
+            raid_obj += str(raid.tier) + "성"
+        text += str(raid.s_time.strftime('%H:%M')) + "~" + str((raid.s_time + timedelta(minutes=45)).strftime('%H:%M')) + " " + str(raid.gym.nick) + " " + raid_obj + "\n"
     if text == "":
         text += "현재 알려진 레이드가 없습니다! 제보하시겠어요?"
     return text
