7264ff4b87d375c98a65bed9a6bc0d418f0866b0
noohes <frostbastard@gmail.com>
2020-02-23 11:35:20 +0900

    commit

--- gokemap/settings.py
+++ gokemap/settings.py
@@ -127,9 +127,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static') #/home/budcha/gokemap/gokemap/static
-STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT = "/home/budcha/gokemap/static"
+# STATIC_DIR = os.path.join(BASE_DIR, 'static') #/home/budcha/gokemap/gokemap/static
+# STATICFILES_DIRS = [STATIC_DIR,]
+# STATIC_ROOT = "/home/budcha/gokemap/static"
 
 #leafmap setting
 #https://django-leaflet.readthedocs.io/en/latest/templates.html#spatial-extent
