c713fd46adc11aadb2642e407ce99a762e6ef0dd
noohes <frostbastard@gmail.com>
2020-02-23 13:00:13 +0900

    ch negative to positive in bad

--- map/skills_restaurant.py
+++ map/skills_restaurant.py
@@ -12,7 +12,7 @@ class randomRestaurant(SkillResponseView):
         name = recommendedRestaurant.name
         good = ratedRestaurant.objects.filter(restaurant=recommendedRestaurant, rating=1).aggregate(Sum('rating'))['rating__sum']
         bad = ratedRestaurant.objects.filter(restaurant=recommendedRestaurant, rating=-1).aggregate(Sum('rating'))['rating__sum']
-        description = f"{recommendedRestaurant.occasion.strip()}  / 👍: {good if good else 0} / 👎: {bad if bad else 0}\n{recommendedRestaurant.place.strip()}\n{recommendedRestaurant.childCategory.strip() if recommendedRestaurant.childCategory else recommendedRestaurant.parentCategory.strip()}"
+        description = f"{recommendedRestaurant.occasion.strip()}  / 👍: {good if good else 0} / 👎: {abs(bad) if bad else 0}\n{recommendedRestaurant.place.strip()}\n{recommendedRestaurant.childCategory.strip() if recommendedRestaurant.childCategory else recommendedRestaurant.parentCategory.strip()}"
         url = recommendedRestaurant.url
         restaurantCard = singleResponse(name, description).web_button("상세보기", url).card()
         positive = {"name":name, "result":1}
